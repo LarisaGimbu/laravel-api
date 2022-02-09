f34e33b837a34a3397dc8ccff3cfa4d6137f7f99
LarisaGimbu <gimbularisa@gmail.com>
2022-02-09 18:56:40 +0100

    stampo posts

--- resources/js/components/Posts.vue
+++ resources/js/components/Posts.vue
@@ -1,13 +1,22 @@
 <template>
 <main class="container">
   <h2>I miei post:</h2>
+  <PostItem 
+    v-for="post in posts"
+    :key="post.id"
+    :post="post" />
 </main>
   
 </template>
 
 <script>
+import PostItem from './partials/PostItem.vue'
+
 export default {
   name: 'Posts',
+  components:{
+    PostItem
+  },
   data(){
     return{
       apiUrl: 'http://127.0.0.1:8000/api/posts',
