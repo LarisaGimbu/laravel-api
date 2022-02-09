83b80618b66a0899b41d1a09f503af7fba8aadfb
LarisaGimbu <gimbularisa@gmail.com>
2022-02-09 18:40:19 +0100

    chiamata axios in posts

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div>
     <Header />
-    <main>Main</main>
+    <Posts />
     <Footer />
   </div>
 </template>
 
 <script>
 import Header from './components/partials/Header.vue'
+import Posts from './components/Posts.vue'
 import Footer from './components/partials/Footer.vue'
 
 export default {
   name: 'App',
   components:{
     Header,
+    Posts,
     Footer
   }
 }
---
+++ resources/js/components/Posts.vue
@@ -0,0 +1,35 @@
+<template>
+<main class="container">
+  <h2>I miei post:</h2>
+</main>
+  
+</template>
+
+<script>
+export default {
+  name: 'Posts',
+  data(){
+    return{
+      apiUrl: 'http://127.0.0.1:8000/api/posts',
+      posts: null,
+    }
+  },
+  mounted(){
+    this.getPosts();
+  },
+  methods:{
+    getPosts(){
+      axios.get(this.apiUrl)
+        .then(res => {
+          this.posts = res.data
+          console.log(res.data);
+        })
+        
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
